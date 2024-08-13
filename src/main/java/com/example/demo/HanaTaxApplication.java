0077920cd666596748e64e0640c816a75356f502
wldnjs7064 <wldnjs7064@naver.com>
2024-08-13 10:20:17 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/example/demo/HanaTaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaTaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaTaxApplication.class, args);
+	}
+
+}
